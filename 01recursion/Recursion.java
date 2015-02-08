1c64b4e66dbc1aafaab9b3d1a4aba165331de654
Wayez <c.wayez3793@gmail.com>
2015-02-08 16:10:33 -0500

    Finished

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -10,31 +10,44 @@ public class Recursion implements hw1{
 	System.out.println(a.fib(3));
 	System.out.println(a.fib(1));
 	System.out.println(a.fib(5));
-	System.out.println(a.fib(-3));
+	System.out.println(a.sqrt(10));
+	System.out.println(a.sqrt(0));
+	System.out.println(a.sqrt(3));
+	System.out.println(a.sqrt(1));
+	System.out.println(a.sqrt(25));
+	System.out.println(a.sqrt(100000000));
+	System.out.println(a.fact(-100000000));
+		
     }
     public String name(){
 	return "Chowdhury, Wayez";
     }
-    public void nNeg(int n){
+    public void nNeg(double n){
 	if (n<0){
 	    throw new IllegalArgumentException();
 	}
     }
     public int fact(int n){
-	nNeg(n);
+	nNeg(n*1.0);
 	if (n>=2)
 	    return n*fact(n-1);
 	return n;
     }
     public int fib(int n){
-	nNeg(n);
+	nNeg(n*1.0);
 	if (n>1)
 	    return fib(n-1)+fib(n-2);
-	else
-	    return n;
+	return n;
     }
 		
     public double sqrt(double n){
-	return n;
+	nNeg(n);
+	return sqrtH(n, n / 2);
+    }
+    public double sqrtH(double n, double guess){
+	if (guess*guess>n-0.00000001 && guess*guess<n+0.00000001)
+	    return guess;
+	return sqrtH(n, (n / guess + guess) / 2);
+
     }
 }
