c718594c6193ccdff8a984c94145f25585c035cb
Wayez <c.wayez3793@gmail.com>
2015-02-08 14:20:33 -0500

    Finished Fib

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,12 +1,40 @@
-public class Recursion{
+public class Recursion implements hw1{
+    public static void main(String[]args){
+	Recursion a=new Recursion();
+	System.out.println(a.name());
+	System.out.println(a.fact(10));
+	System.out.println(a.fact(0));
+	System.out.println(a.fact(3));
+	System.out.println(a.fib(10));
+	System.out.println(a.fib(0));
+	System.out.println(a.fib(3));
+	System.out.println(a.fib(1));
+	System.out.println(a.fib(5));
+	System.out.println(a.fib(-3));
+    }
     public String name(){
 	return "Chowdhury, Wayez";
     }
+    public void nNeg(int n){
+	if (n<0){
+	    throw new IllegalArgumentException();
+	}
+    }
     public int fact(int n){
-	if (n>1)
+	nNeg(n);
+	if (n>=2)
 	    return n*fact(n-1);
 	return n;
     }
-    //public int fib(int n){
-	
+    public int fib(int n){
+	nNeg(n);
+	if (n>1)
+	    return fib(n-1)+fib(n-2);
+	else
+	    return n;
+    }
+		
+    public double sqrt(double n){
+	return n;
+    }
 }
