e96d988783bbb21a4fc2bb8cfc13fb0630d6989d
Wayez <c.wayez3793@gmail.com>
2015-02-08 13:35:23 -0500

    Finished Factorial

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,12 @@
+public class Recursion{
+    public String name(){
+	return "Chowdhury, Wayez";
+    }
+    public int fact(int n){
+	if (n>1)
+	    return n*fact(n-1);
+	return n;
+    }
+    //public int fib(int n){
+	
+}
