12dad8e839d3fd472a0f9e052533342878559427
Wayez <c.wayez3793@gmail.com>
2015-02-26 22:44:01 -0500

    NQueens Woo

--- 02awesomerRecursion/KDriver.java
+++ 02awesomerRecursion/KDriver.java
@@ -1,7 +1,7 @@
 public class KDriver{
 
     public static void main(String[]arrr){
-	KnightsTour t = new KnightsTour(8);
+	KnightsTour t = new KnightsTour(6);
 	if(t.solve()){
 		System.out.println(t);
 	}else{
