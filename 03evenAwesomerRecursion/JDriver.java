f70c86a5da298909ffb09c91452384d2975da022
Wayez <c.wayez3793@gmail.com>
2015-02-26 23:42:50 -0500

    Almost done

--- 03evenAwesomerRecursion/JDriver.java
+++
@@ -1,7 +0,0 @@
-public class QDriver1{
-    public static void main(String[]arrr){
-     NQueens t = new NQueens(5);
-     System.out.println(t.name());
-     System.out.println("NQueens:");
-    }
-}
