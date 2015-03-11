44d06fee12c9dd857e49bcaf35d5086c653bce62
Wayez Chowdhury <c.wayez3793@gmail.com>
2015-03-11 13:10:14 -0400

    Finished LNode.java. Began MyLinkedList.java

---
+++ 07linkedList/LNode.java
@@ -0,0 +1,23 @@
+public class LNode{
+    private int value;
+    private LNode next;
+    public LNode(int value){
+	setValue(value);
+    }
+    public void setValue(int value){
+	this.value=value;
+    }
+    public int getValue(){
+	return value;
+    }
+    public void setNext(){
+	this.next=next;
+    }
+    public LNode getNext(){
+	return next;
+    }
+    public String toString(){
+	return Integer.toString(value) + ",";
+	//the comma to annoy mr k
+    }
+}
