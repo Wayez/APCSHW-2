95c146c84f689574a97f8d3d163952b09ef409c5
Wayez Chowdhury <c.wayez3793@gmail.com>
2015-04-02 13:12:57 -0400

    Compile errors all around

--- 11BFFs/Maze.java
+++ 11BFFs/Maze.java
@@ -9,9 +9,6 @@ public class Maze{
     private int maxx,maxy;
     private int startx,starty;
     //Terminal keycodes to clear the terminal, or hide/show the cursor
-    private String clear =  "\033[2J";
-    private String hide =  "\033[?25l";
-    private String show =  "\033[?25h";
 
     /** Same constructor as before...*/
     public Maze(String filename){  
