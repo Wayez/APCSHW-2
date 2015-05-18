d0316d8fe1d9c1e00ec1eb8227cd50d1f4c66b47
Wayez <c.wayez3793@gmail.com>
2015-05-18 02:51:05 +0000

    Time to test

--- 14Medianisi/RunningMedian.java
+++ 14Medianisi/RunningMedian.java
@@ -17,6 +17,31 @@ public class RunningMedian{
             return heap1.peek();
         return heap2.peek();
     }
-    
+    public void add(int x){
+        if (heap1.get(0) == 0 && heap2.get(0) == 0)
+            heap1.add(x);
+        else if (heap1.get(0) == heap2.get(0)){
+            if (x < heap1.peek())
+                heap1.add(x);
+            else
+                heap2.add(x);
+        }
+        else if (heap1.get(0) > heap2.get(0)){
+            if (x > heap2.peek())
+                heap2.add(x);
+            else {
+                heap2.add(heap1.remove());
+                heap1.add(x);
+            }
+        }
+        else{
+            if (x < heap1.peek())
+                heap1.add(x);
+            else {
+                heap1.add(heap2.remove());
+                heap2.add(x);
+            }
+        }
+    }
     
 }
